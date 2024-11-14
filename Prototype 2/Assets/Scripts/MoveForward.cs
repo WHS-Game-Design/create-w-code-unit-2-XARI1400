9196fe139583dc7e98c02aaf0051cc1e550b9fa2
XARI <xaribeforeko@gmail.com>
2024-11-14 15:46:56 -0800

    TY

--- Prototype 2/Assets/Scripts/MoveForward.cs
+++ Prototype 2/Assets/Scripts/MoveForward.cs
@@ -13,12 +13,11 @@ public class MoveForward : MonoBehaviour
         
     }
 
-    private float speed = 40.0f;
+    public float speed = 40.0f;
 
     // Update is called once per frame
     void Update()
     {
-
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
